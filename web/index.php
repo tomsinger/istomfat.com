a97d35505abbef973dcef18431f9deb001934dd4
Tom Singer <tomsinger@gmail.com>
2013-08-09 15:06:14 +0000

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+ <title>Is Tom Fat?</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+  <h1 style="text-align: center">Yes!</h1>
+  <div>
+    If it's Tom Dickin that is, Tom Singer is not!
+  </div> 
+</body>
+</html>
